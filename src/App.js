4790c16615bcf93755ed2c293e2ea96730b7daaa
Surender Chahal <surender.chahal@move.com>
2021-03-01 16:54:36 -0800

    Draft version of app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Dear Home Buyers/Sellers,
+          <br />
+          Welcome to ZERO Realtor, we gonna save your money.
+          <br />
+          --Chahal
+      </header>
+    </div>
+  );
+}
+
+export default App;
